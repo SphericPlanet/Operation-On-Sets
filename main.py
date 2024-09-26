c9ccc0a217d74f434cac2f936812831cee7f468a
SphericPlanet <gauranshpandey09@gmail.com>
2024-09-26 18:38:05 +0530

    Operations On Set

---
+++ main.py
@@ -0,0 +1,10 @@
+set1 = {1, 4, 5, 1, 2, 6}
+print(set1)
+set1.add(3)
+print(set1)
+
+setA = {1, 2, 3, 4}
+setB = {3, 4, 5, 6}
+
+print("Differences:", setA.difference(setB))
+print("Symmetric Differences:", setA.symmetric_difference(setB))
